6460aba66740f14b7c919cded6b853252ad0840d
devysf <ozacetyusuf@gmail.com>
2018-07-10 12:32:33 +0300

    user profile page is designed.

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 var passport = require("passport");
 var User = require("../models/user");
+var Book = require("../models/book");
 
 //home page route
 router.get("/",function(req,res){
@@ -18,7 +19,15 @@ router.get("/register",function(req,res){
 
 //sign up logic
 router.post("/register",function(req,res){
-  var newUser = new User({username : req.body.username });
+
+  var newUser = new User({
+    username : req.body.username,
+    firstname: req.body.firstname,
+    lastname : req.body.lastname,
+    email: req.body.email,
+    avatar : req.body.avatar
+  });
+
   User.register(newUser,req.body.password,function(err,user){
     if(err){
       console.log(err);
@@ -55,6 +64,26 @@ router.get("/logout",function(req,res){
   req.flash("success", "Logged you out!");
   req.logout();
   res.redirect("/books");
+});
+
+
+
+//USER PROFILE
+router.get("/users/:id",function(req,res){
+  User.findById(req.params.id, function(err, foundUser){
+    if(err){
+      req.flash("error","Something went wrong");
+      res.redirect("/");
+    }
+
+    Book.find().where("author.id").equals(foundUser._id).exec(function(err,foundBooks){
+      if(err){
+        req.flash("error","Something went wrong");
+        res.redirect("/");
+      }
+      res.render("users/show",{user: foundUser, books: foundBooks});
+    });
+  });
 });
 
 module.exports = router;
