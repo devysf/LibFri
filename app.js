2addad149c0dacb04c6db23e80ca8e58f5d28a6b
devysf <ozacetyusuf@gmail.com>
2019-02-14 14:29:44 +0300

    Mongolab settings is done.

--- app.js
+++ app.js
@@ -1,28 +1,34 @@
-var express     = require("express"),
-    app         = express(),
-    bodyParser  = require("body-parser"),
-    methodOverride = require("method-override"),
-    flash       = require("connect-flash"),
-    mongoose    = require("mongoose"),
-    passport    = require("passport"),
-    LocalStrategy = require("passport-local"),
-    expressSession = require("express-session"),
-    expressValidator = require('express-validator'),
-    Book        = require("./models/book"),
-    Comment     = require("./models/comment"),
-    User        = require("./models/user"),
-    seedDb      = require("./seeds");
-    require('dotenv').config()
+var express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  methodOverride = require("method-override"),
+  flash = require("connect-flash"),
+  mongoose = require("mongoose"),
+  passport = require("passport"),
+  LocalStrategy = require("passport-local"),
+  expressSession = require("express-session"),
+  expressValidator = require("express-validator"),
+  Book = require("./models/book"),
+  Comment = require("./models/comment"),
+  User = require("./models/user"),
+  seedDb = require("./seeds");
+require("dotenv").config();
 
 // importing ROUTES
 var commentRoutes = require("./routes/comments"),
-    bookRoutes    = require("./routes/books"),
-    indexRoutes   = require("./routes/index");
+  bookRoutes = require("./routes/books"),
+  indexRoutes = require("./routes/index");
 
+const mongoDB = require("./config/mongodbKeys").mongoURI;
+mongoose
+  .connect(mongoDB)
+  .then(() => console.log("Mongodb connected"))
+  .catch(err => console.log(err));
 
-mongoose.connect("mongodb://localhost/lib_fri");
+//mongoose.connect("mongodb://localhost/lib_fri");
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
@@ -33,11 +39,13 @@ app.use(flash());
 
 //PASSPORT.JS Congiguration
 
-app.use(expressSession({
-  secret : "This is a keyword to help hashing your password.",
-  resave : false,
-  saveUninitialized : false
-}));
+app.use(
+  expressSession({
+    secret: "This is a keyword to help hashing your password.",
+    resave: false,
+    saveUninitialized: false
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -47,9 +55,8 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
 //we pass this variable template pages to use this variable in that file. then we can easily control which user is logged in
-app.use(function(req,res,next){
+app.use(function(req, res, next) {
   res.locals.currentUser = req.user;
   res.locals.error = req.flash("error");
   res.locals.success = req.flash("success");
@@ -59,31 +66,32 @@ app.use(function(req,res,next){
 
 //Now moment is available for use in all of your view files
 //via the variable named moment
-app.locals.moment = require('moment');
+app.locals.moment = require("moment");
 
 // Validator settings
-app.use(expressValidator({
-  errorFormatter: function(param, msg, value) {
-      var namespace = param.split('.')
-      , root    = namespace.shift()
-      , formParam = root;
+app.use(
+  expressValidator({
+    errorFormatter: function(param, msg, value) {
+      var namespace = param.split("."),
+        root = namespace.shift(),
+        formParam = root;
 
-    while(namespace.length) {
-      formParam += '[' + namespace.shift() + ']';
+      while (namespace.length) {
+        formParam += "[" + namespace.shift() + "]";
+      }
+      return {
+        param: formParam,
+        msg: msg,
+        value: value
+      };
     }
-    return {
-      param : formParam,
-      msg   : msg,
-      value : value
-    };
-  }
-}));
+  })
+);
 
-app.use("/",indexRoutes);
-app.use("/books",bookRoutes);
-app.use("/books/:id/comments",commentRoutes);
+app.use("/", indexRoutes);
+app.use("/books", bookRoutes);
+app.use("/books/:id/comments", commentRoutes);
 
-
-app.listen(3000,function(){
+app.listen(3000, function() {
   console.log("The server has started");
 });
